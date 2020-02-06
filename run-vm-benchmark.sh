f4d26ebcc45340ff2edc3531f1ae441d7d6e0f68
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2020-02-06 16:21:05 +0300

    Run on full 5 minutes

--- run-vm-benchmark.sh
+++ run-vm-benchmark.sh
@@ -9,6 +9,13 @@ rm -rf results testfile
 mkdir -p results
 echo "OK"
 
+echo "Waiting for next full 5 minutes"
+fix=300
+date
+sleep_for=$((fix - $(date +%s) % fix))
+echo "Will wait $sleep_for seconds"
+sleep ${sleep_for}
+
 echo "Running benchmark..."
 ./benchmark_script/bench-fio.sh benchmark_script/fio-job-template-file.fio results . testfile 20G
 echo "DONE"
